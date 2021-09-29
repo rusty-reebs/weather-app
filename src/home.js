d68abce4b5bd9ed5d6a41e1edee447f56953a3ce
rusty-reebs <rusty_reebs@tutanota.com>
2021-09-29 12:02:39 +0400

    added conditionals for weather images and days of the week, completed form input

--- src/home.js
+++ src/home.js
@@ -1,4 +1,8 @@
 // renderHome
+
+//? put all these in an object domElements, then export the object?
+const form = document.createElement("form");
+const input = document.createElement("input");
 const city = document.createElement("h2");
 const date = document.createElement("h3");
 const currentTemp = document.createElement("h1");
@@ -6,10 +10,12 @@ const feelsLike = document.createElement("p");
 const currentDescrip = document.createElement("h4");
 const currentDescripPic = document.createElement("img");
 const windSpeed = document.createElement("h4");
-const tomorrowTemp = document.createElement("h4");
 const tomorrowDate = document.createElement("h4");
-const nextDayTemp = document.createElement("h4");
+const tomorrowTemp = document.createElement("h4");
+const tomorrowPic = document.createElement("img");
 const nextDayDate = document.createElement("h4");
+const nextDayTemp = document.createElement("h4");
+const nextDayPic = document.createElement("img");
 
 const renderHome = () => {
   const content = document.getElementById("content");
@@ -18,14 +24,11 @@ const renderHome = () => {
 
   const upperDiv = document.createElement("div");
   upperDiv.classList.add("upper");
-  const form = document.createElement("form");
-  const input = document.createElement("input");
   input.classList.add("searchbar");
   form.appendChild(input);
 
   const dateDiv = document.createElement("div");
   date.classList.add("date");
-  date.innerHTML = "September 24, 2021 8:40 am"; // needs to be modified in index.js
   dateDiv.appendChild(date);
 
   const cityDiv = document.createElement("div");
@@ -42,7 +45,6 @@ const renderHome = () => {
 
   const currentDescripDiv = document.createElement("div");
   currentDescripDiv.classList.add("descrip");
-  currentDescripPic.src = "../src/img/01d.png";
   currentDescripPic.height = "100";
   currentDescripDiv.appendChild(currentDescripPic);
   currentDescripDiv.appendChild(currentDescrip);
@@ -50,8 +52,6 @@ const renderHome = () => {
 
   const tomorrowDiv = document.createElement("div");
   tomorrowDiv.classList.add("tomorrow");
-  const tomorrowPic = document.createElement("img");
-  tomorrowPic.src = "../src/img/02d.png";
   tomorrowPic.height = "80";
   tomorrowDiv.appendChild(tomorrowDate);
   tomorrowDiv.appendChild(tomorrowPic);
@@ -59,8 +59,6 @@ const renderHome = () => {
 
   const nextDayDiv = document.createElement("div");
   nextDayDiv.classList.add("nextday");
-  const nextDayPic = document.createElement("img");
-  nextDayPic.src = "../src/img/02d.png";
   nextDayPic.height = "80";
   nextDayDiv.appendChild(nextDayDate);
   nextDayDiv.appendChild(nextDayPic);
@@ -79,11 +77,23 @@ const renderHome = () => {
   content.appendChild(container);
 };
 
-export { renderHome, city, currentTemp, feelsLike, currentDescrip, windSpeed };
+export {
+  renderHome,
+  form,
+  input,
+  date,
+  city,
+  currentTemp,
+  feelsLike,
+  currentDescrip,
+  windSpeed,
+};
 export {
   currentDescripPic,
   tomorrowTemp,
   tomorrowDate,
+  tomorrowPic,
   nextDayTemp,
   nextDayDate,
+  nextDayPic,
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import "./styles/style.css";
 import { renderHome } from "./home";
 import {
+  form,
+  input,
+  date,
   city,
   currentTemp,
   feelsLike,
@@ -8,7 +11,14 @@ import {
   windSpeed,
   currentDescripPic,
 } from "./home";
-import { tomorrowTemp, tomorrowDate, nextDayTemp, nextDayDate } from "./home";
+import {
+  tomorrowTemp,
+  tomorrowDate,
+  tomorrowPic,
+  nextDayTemp,
+  nextDayDate,
+  nextDayPic,
+} from "./home";
 
 let cityLat;
 let cityLon;
@@ -43,10 +53,17 @@ const getWeather = async (lat, lon) => {
 
 renderHome();
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  masterFunc(input.value);
+  form.reset();
+});
+
 const masterFunc = async (cityname) => {
   await getCityLatLon(cityname);
   await getWeather(cityLat, cityLon);
   refineDataObject();
+  date.innerHTML = refinedAppData.currentTime;
   city.innerHTML = cityname;
   currentTemp.innerHTML = refinedAppData.currentTemp + "&deg;" + "C";
   feelsLike.innerHTML =
@@ -55,13 +72,12 @@ const masterFunc = async (cityname) => {
   windSpeed.innerHTML = "Wind: " + refinedAppData.windSpeed + " km/h";
   currentDescripPic.src = getImageFromId(refinedAppData.currentId);
   tomorrowTemp.innerHTML = refinedAppData.tomorrowTemp + "&deg;" + "C";
-  // tomorrowDate.innerHTML = refinedAppData.tomorrowDate;
   tomorrowDate.innerHTML = "Tomorrow";
+  tomorrowPic.src = getImageFromId(refinedAppData.tomorrowId);
   nextDayTemp.innerHTML = refinedAppData.nextDayTemp + "&deg;" + "C";
   nextDayDate.innerHTML = refinedAppData.nextDayDate;
+  nextDayPic.src = getImageFromId(refinedAppData.nextDayId);
 };
-
-masterFunc("Medellin");
 
 // functions to refine data
 
@@ -82,7 +98,8 @@ const capitalizeString = (data) => {
 let refinedAppData = {};
 
 const refineDataObject = () => {
-  const currentTime = new Date(weatherData.current.dt);
+  const currentTime = new Date(weatherData.current.dt * 1000);
+  const refinedCurrentTime = currentTime.toLocaleString();
   const currentTemp = makeRoundNumber(weatherData.current.temp);
   const feelsLike = makeRoundNumber(weatherData.current.feels_like);
   const currentDescripCaps = capitalizeString(
@@ -94,14 +111,36 @@ const refineDataObject = () => {
   const tomorrowDescripCaps = capitalizeString(
     weatherData.daily[1].weather[0].description
   );
-  const nextDayDate = new Date(weatherData.daily[2].dt);
-  const nextDayName = nextDayDate.getDay();
+  const nextDayDate = new Date(weatherData.daily[2].dt * 1000);
+  let nextDayName = nextDayDate.getDay();
+  switch (nextDayName) {
+    case 0:
+      nextDayName = "Sunday";
+      break;
+    case 1:
+      nextDayName = "Monday";
+      break;
+    case 2:
+      nextDayName = "Tuesday";
+      break;
+    case 3:
+      nextDayName = "Wednesday";
+      break;
+    case 4:
+      nextDayName = "Thursday";
+      break;
+    case 5:
+      nextDayName = "Friday";
+      break;
+    case 6:
+      nextDayName = "Saturday";
+  }
   const nextDayTemp = makeRoundNumber(weatherData.daily[2].temp.day);
   const nextDayDescripCaps = capitalizeString(
     weatherData.daily[2].weather[0].description
   );
 
-  refinedAppData.currentTime = currentTime;
+  refinedAppData.currentTime = refinedCurrentTime;
   refinedAppData.currentTemp = currentTemp;
   refinedAppData.feelsLike = feelsLike;
   refinedAppData.currentDescrip = currentDescripCaps;
@@ -120,7 +159,38 @@ const refineDataObject = () => {
 };
 
 const getImageFromId = (id) => {
-  if (id == 741) {
+  const idString = id.toString();
+  if (idString[0] == "2") {
+    return "../src/img/11d.png";
+  }
+  if (idString[0] == "3") {
+    return "../src/img/09d.png";
+  }
+  if (idString[0] == "5" && idString[1] == "0") {
+    return "../src/img/10d.png";
+  }
+  if (idString[0] == "5" && idString[1] == "1") {
+    return "../src/img/13d.png";
+  }
+  if ((idString[0] == "5" && idString[1] == "2") || idString[1] == "3") {
+    return "../src/img/09d.png";
+  }
+  if (idString[0] == "6") {
+    return "../src/img/13d.png";
+  }
+  if (idString[0] == "7") {
     return "../src/img/50d.png";
+  }
+  if (idString == "800") {
+    return "../src/img/01d.png";
+  }
+  if (idString == "801") {
+    return "../src/img/02d.png";
+  }
+  if (idString == "802") {
+    return "../src/img/03d.png";
+  }
+  if (idString == "803" || idString == "804") {
+    return "../src/img/04d.png";
   }
 };
